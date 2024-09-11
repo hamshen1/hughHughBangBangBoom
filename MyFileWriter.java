f02932ed030efded8f718ea38d829fe3ee2de991
hamshen1 <hamshen1@hwemail.com>
2024-09-11 09:50:56 -0700

    Merge pull request #2 from hamshen1/filesize
    FileSize

---
+++ MyFileWriter.java
@@ -0,0 +1,17 @@
+import java.nio.file;
+
+class MyFileWriter {
+
+    //Prints the size of file in bytes
+    public static void printFileSize(String fileName) {
+        File fileyFileFile = new File(fileName);
+        
+        System.out.println(fileyFileFile.length());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ASDLKASJDLKASJD");
+        printFileSize("example1.txt");
+
+     }
+}
