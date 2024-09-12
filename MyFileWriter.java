579e6589c04646090ca95beb4ff98159d300f77d
Tyler <tanderson1@hwemail.com>
2024-09-12 08:32:09 -0700

    Added printTotalFileSize method (#4)
    Thanks project manager <3 much love 
    
    -hughHughBangBangBoom

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,4 +1,4 @@
-import java.nio.file;
+import java.io.File;
 
 class MyFileWriter {
 
@@ -12,6 +12,24 @@ class MyFileWriter {
     public static void main(String[] args) {
         System.out.println("ASDLKASJDLKASJD");
         printFileSize("example1.txt");
-
+        testPrintTotalFileSize();
      }
+
+     private static void printTotalFileSize(String... fileNames) {
+        long totalSize = 0;
+        for (String fileName : fileNames) {
+            File file = new File(fileName);
+            if (file.exists()) {
+                totalSize += file.length();
+            }
+        }
+        System.out.println("Total size of all files: " + totalSize + " bytes");
+    }
+
+    private static void testPrintTotalFileSize () {
+        FileWriteExample.main(new String[0]);
+        printTotalFileSize("example1.txt", "example2.txt","example3.txt","example4.txt","example5.txt");
+        System.out.println("worked if # was 65 bytes");
+    }
+    
 }
